b19e542f46363dc4596640f96b5f10910d23c302
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2025-01-10 23:12:49 +0700

    .

--- depdalp/resources/views/deposit.blade.php
+++ depdalp/resources/views/deposit.blade.php
@@ -85,7 +85,7 @@
         <div class="pt-4">
             <button
                 type="submit"
-                class="w-full bg-blue-600 text-white font-medium py-2 px-4 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                class="w-full bg-red-600 text-white font-medium py-2 px-4 rounded-lg shadow hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
                 Submit Deposit Request
             </button>
         </div>
